311e4c083fc64427a5e8cc0ba7256fb2fa66b202
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-01-30 20:33:16 +0900

    feat: add return project

---
+++ return/app/src/main/java/jp/kaleidot725/navgraph/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.kaleidot725.navgraph
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
