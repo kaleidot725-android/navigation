20a44b74db8ac6994483bcef7cd4b9dcf54866cb
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-01-30 20:44:02 +0900

    feat: add new fragment

---
+++ return/app/src/main/java/jp/kaleidot725/navgraph/SecondFragment.kt
@@ -0,0 +1,9 @@
+package jp.kaleidot725.navgraph
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SecondFragment : Fragment(R.layout.fragment_second)
